082c3ec4c8fe0f06d8614ace457cbd1f4ac82cb4
shumynvolodymyr <shumivova@gmail.com>
2021-08-17 15:17:32 +0300

    hw_4

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,6 +1,9 @@
 import {saveCar} from "../../API/postCar.api";
+import {useState} from "react";
 
 export default function Form() {
+
+ let [formState, setFormState] = useState({model:'', price:'', year:''});
 
     let onFormSubmit = (e) => {
         e.preventDefault();
@@ -8,13 +11,17 @@ export default function Form() {
         saveCar(carToSave);
     };
 
+    function onFormChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value})
+    }
+
     return (
         <div className={'form'}>
             <h1>ADD AUTO:</h1>
             <form onSubmit={onFormSubmit}>
-                <input type="text" name={'model'} placeholder={'Model'} maxLength={20}/>
-                <input type="number" name={'price'} placeholder={'Price'} min={0}/>
-                <input type="number" name={'year'} placeholder={'Year'} min={1990} max={2021}/>
+                <input type="text" name={'model'} value={formState.model} placeholder={'Model'} maxLength={20} onChange={onFormChange}/>
+                <input type="number" name={'price'} value={formState.price} placeholder={'Price'} min={0} onChange={onFormChange}/>
+                <input type="number" name={'year'} value={formState.year} placeholder={'Year'} min={1990} max={2021} onChange={onFormChange}/>
                 <input type="submit" name={'submit'} value={'SAVE'}/>
             </form>
         </div>
