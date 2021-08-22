df24ac9d4648523b07cc6616633719362d40d615
shumynvolodymyr <shumivova@gmail.com>
2021-08-22 14:52:08 +0300

    test

--- src/API/getCars.api.js
+++
@@ -1,3 +0,0 @@
-export const getCars = () => {
-    return fetch('http://195.72.146.25/api/v1/cars').then(value => value.json())
-}
--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
 import './App.css';
-import Cars from "./components/cars/Cars";
+import {createCar, getCar} from "./services/services/car.servise";
+import {useEffect, useState} from "react";
 
 function App() {
+
+    let [cars, setCars] = useState([]);
+    let [model, setModel] = useState('');
+    let [price, setPrice] = useState('');
+    let [year, setYear] = useState('');
+
+
+    const carInfo = {
+        model: model,
+        price: price,
+        year: year
+    }
+
+    const save = (e) => {
+        e.preventDefault()
+        createCar(carInfo)
+    }
+
+
+    useEffect(() => {
+        getCar().then(value => setCars([...value]))
+    }, [cars])
+
+    // console.log(cars)
+
+    let onModelChange = (e) => {
+        setModel(e.target.value)
+    };
+    let onPriceChange = (e) => {
+        setPrice(e.target.value)
+    };
+    let onYearChange = (e) => {
+        setYear(e.target.value)
+    };
+
     return (
-        <div>
-            <Cars/>
+        <div className={'app'}>
+            <form onSubmit={save}>
+                <input type="text" name={'model'} placeholder={'Model'} onChange={onModelChange}/>
+                <input type="number" name={'price'} placeholder={'Price'} onChange={onPriceChange}/>
+                <input type="number" name={'year'} placeholder={'Year'} onChange={onYearChange}/>
+                <input type="submit" name={'submit'} value={'SAVE'}/>
+            </form>
+
+            <h1>CARS:</h1>
+            {
+                cars.map(value => <div key={value.id}>
+                    <h3>{value.id}. {value.model}</h3>
+                </div>)
+            }
         </div>
     );
 }
