961736991b513b94cab5f0cb8dbb1070880dbf5a
shumynvolodymyr <shumivova@gmail.com>
2021-08-23 17:12:51 +0300

    hw_6

--- src/components/postDetails/PostDetails.js
+++ src/components/postDetails/PostDetails.js
@@ -1,12 +1,11 @@
 import './PostDetails.css'
 
-export default function PostDetails({location:{state}}) {
-
+export default function PostDetails({location: {state}}) {
+    const {id, title, body} = state;
     return (
         <div className={'post-info'}>
-            {
-                JSON.stringify(state)
-            }
+            <h2>{id}. {title}</h2>
+            <p><i>Body: {body}</i></p>
         </div>
     );
 }
