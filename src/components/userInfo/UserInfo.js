280288df5addfed6336f738f69645e345f7157e1
shumynvolodymyr <shumivova@gmail.com>
2021-09-06 12:22:19 +0300

    react project

---
+++ src/components/userInfo/UserInfo.js
@@ -0,0 +1,8 @@
+import './UserInfo.css'
+export default function UserInfo() {
+    return (
+        <div className={'registration'}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHP-RWRh1rrTqmNr47cNGPmfmOLTy9g-yZAg&usqp=CAU" alt=""/>
+        </div>
+    );
+}
