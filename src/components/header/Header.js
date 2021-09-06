4bf0782eedb4ad99ca18f2747bd010c9d238e2b5
shumynvolodymyr <shumivova@gmail.com>
2021-09-06 11:48:18 +0300

    checkbox theme

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,10 +5,11 @@ export default function Header() {
 
 
     return (
-<>
+<div className={'header'}>
         <Checkbox/>
+    <img className={'logo'} src='https://images.squarespace-cdn.com/content/v1/60186ab2f6d4492a8192dc46/1616686606228-72NATDX13LCR9R5PLZAB/mobileOxfilm+logo+Full+color.png?format=1500w' alt=""/>
 
 
-</>
+</div>
     );
 }
